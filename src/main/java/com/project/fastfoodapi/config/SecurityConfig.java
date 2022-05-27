9f1371e22f7d42879addfa6aea31ea48cdcb08c2
umidjon-2231 <83820489+umidjon-2231@users.noreply.github.com>
2022-05-27 15:04:05 +0500

    security optimization

--- src/main/java/com/project/fastfoodapi/config/SecurityConfig.java
+++ src/main/java/com/project/fastfoodapi/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.project.fastfoodapi.config;
 import com.project.fastfoodapi.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
@@ -18,9 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/assets/**").permitAll()
                 .antMatchers("/api/**").authenticated()
